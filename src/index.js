75a639e27dbcaf11ecbdbdfddd3f9269cf44c266
andrew <neon.duncan@gmail.com>
2021-06-15 19:13:02 -0500

    commit tree 21f39f98d2a4f0649dc976b0a245a47a296bc6dc

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
-const init = () => {
   
+const baseUrl = "http://localhost:3000/movies/"
+
+const init = () => {
+  const inputForm = document.querySelector('form')
+
+  inputForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const input = document.querySelector('input#searchByID')
+
+    fetch(`${baseUrl}${input.value}`)
+    .then(response => response.json())
+    .then(data => {
+      const h4 = document.querySelector("#movieDetails h4")
+      const p = document.querySelector("#movieDetails p")
+      h4.innerText = data.title
+      p.innerText = data.summary
+    })
+  })
+
+
 }
 
 document.addEventListener('DOMContentLoaded', init);
